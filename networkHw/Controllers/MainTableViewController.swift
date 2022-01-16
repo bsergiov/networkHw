16b7875edd30783da82b215e84674723e34784eb
bsergiov <81264661+bsergiov@users.noreply.github.com>
2022-01-16 22:13:18 +0200

    added error handling

--- networkHw/Controllers/MainTableViewController.swift
+++ networkHw/Controllers/MainTableViewController.swift
@@ -8,29 +8,38 @@
 import UIKit
 
 class MainTableViewController: UITableViewController {
-
-    // MARK: - IB Outlets
     
     // MARK: - Private Properties
     private let networkManager = NetworkManager()
+    
     private let urlString = "https://rustybeer.herokuapp.com/styles"
     
     private var listBeer: [BeerModel] = []
     
+    private let indicatorView = UIActivityIndicatorView()
+    
+    
     // MARK: - Life Cicle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupIndicatorView()
+        
         NetworkDataFetcher(networkManager: networkManager).fetchJsonData(urlString: urlString) { listObjects in
-            guard let listObjects = listObjects else { return }
+            guard let listObjects = listObjects else {
+                self.indicatorView.stopAnimating()
+                self.showAllert()
+                return
+            }
 
             self.listBeer = listObjects
             self.tableView.reloadData()
+            
+            self.indicatorView.stopAnimating()
         }
-    
     }
 
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -40,7 +49,6 @@ class MainTableViewController: UITableViewController {
         return listBeer.count
     }
 
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CustomTableViewCell", for: indexPath) as! CustomTableViewCell
 
@@ -52,51 +60,22 @@ class MainTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
+}
+
+// MARK: - Private Methodes
+extension MainTableViewController {
+    private func setupIndicatorView(){
+        indicatorView.center = view.center
+        view.addSubview(indicatorView)
+        
+        indicatorView.startAnimating()
+        indicatorView.hidesWhenStopped = true
+    }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    private func showAllert() {
+        let allert = UIAlertController(title: "Server Error", message: "Server error, sorry", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        allert.addAction(action)
+        
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
