da84dd77faa6d87fe9943c5fc5f8800a744b2eea
bsergiov <81264661+bsergiov@users.noreply.github.com>
2022-01-17 09:13:14 +0200

    refactor CustomTableViewCell.swift

--- networkHw/Controllers/CustomTableViewCell.swift
+++ networkHw/Controllers/CustomTableViewCell.swift
@@ -15,21 +15,9 @@ class CustomTableViewCell: UITableViewCell {
     
     
     // MARK: - Public Methodes
-    
     func setCell(beerModel: BeerModel) {
         nameBeerLabel.text = beerModel.name
         discriptionBeerLabel.text = beerModel.description
-    }
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
